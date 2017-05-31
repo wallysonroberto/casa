177ce3e0a8a4d9fd92f6a8f1bcd09f682e90ce2b
Wallyson Roberto <wallyson.16@live.com>
2017-05-31 17:27:06 -0300

    Slides de Lógica matemática

---
+++ curso python/udemy/seção 1/aula 8.py
@@ -0,0 +1,7 @@
+#concatenação de strings
+nome="Wallyson "
+sobrenome="Roberto"
+nomecompleto=nome+sobrenome;
+print(nomecompleto);
+
+
---
+++ curso python/udemy/seção 1/aula8.py
@@ -0,0 +1,8 @@
+#concatenação de strings
+nome="Wallyson"
+sobrenome="Roberto"
+nomecompleto=nome+" "+sobrenome;
+print(nomecompleto);
+print(nome*9)
+
+
